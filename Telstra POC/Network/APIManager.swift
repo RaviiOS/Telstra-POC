aef300db33d0b2e23ddd8a34334554d4ccadccdf
Ravi Kumar Yaganti <ryaganti@sharkdreams.in>
2020-07-19 11:33:44 +0530

    # updated unit test

--- Telstra POC/Network/APIManager.swift
+++ Telstra POC/Network/APIManager.swift
@@ -80,4 +80,5 @@ class APIManager {
         }
         task.resume()
     }
+
 }
--- Telstra POCTests/HttpClientTests.swift
+++ Telstra POCTests/HttpClientTests.swift
@@ -41,34 +41,25 @@ class HttpClientTests: XCTestCase {
 //        // Assert
 //    }
 
-    // Asynchronous test: success fast, failure slow
-    func testValidCallToiTunesGetsHTTPStatusCode200() {
-      // given
+    
+    func testCountryDetailsAPI() {
         let urlString = APIManager.networkEnviroment.rawValue + EndPoints.fetchCountryDetails.rawValue
       let url =
         URL(string: urlString)
-      // 1
-      let promise = expectation(description: "Status code: 200")
+      let promise = expectation(description: "Completion handler invoked")
+      var statusCode: Int?
+      var responseError: Error?
 
-      // when
       let dataTask = sut.dataTask(with: url!) { data, response, error in
-        // then
-        if let error = error {
-          XCTFail("Error: \(error.localizedDescription)")
-          return
-        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-          if statusCode == 200 {
-            // 2
-            promise.fulfill()
-          } else {
-            XCTFail("Status code: \(statusCode)")
-          }
-        }
+        statusCode = (response as? HTTPURLResponse)?.statusCode
+        responseError = error
+        promise.fulfill()
       }
       dataTask.resume()
-      // 3
       wait(for: [promise], timeout: 5)
-    }
 
+      XCTAssertNil(responseError)
+      XCTAssertEqual(statusCode, 200)
+    }
 
 }
